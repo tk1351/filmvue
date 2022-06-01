0d052806c2c7ec28e22e26ee23de4867adea7f04
tk1351 <takakikitsui0111@gmail.com>
2022-06-02 07:44:52 +0900

    feat: Boxレイアウト追加

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import Header from './components/atoms/Header/index.vue'
 import Footer from './components/atoms/Footer/index.vue'
+import Box from './components/atoms/Box/index.vue'
 </script>
 
 <template>
   <Header />
   <Suspense>
     <main class="main-content">
-      <router-view></router-view>
+      <Box padding="40px 80px">
+        <router-view></router-view>
+      </Box>
     </main>
   </Suspense>
   <Footer />
---
+++ src/components/atoms/Box/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ padding?: string }>(), {
+  padding: '',
+})
+</script>
+
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box {
+  padding: v-bind(padding);
+}
+</style>
